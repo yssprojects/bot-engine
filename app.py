d637b378b1cb8cff6e97c4da39dbbc0bb4f4c0d1
yssprojects <76963311+yssprojects@users.noreply.github.com>
2021-01-26 10:54:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import schedule
+import time
+import requests
+
+def job():
+    r = requests.get('http://tgfile.stream.seshu.co/').content
+    print(r)
+schedule.every(1).seconds.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
